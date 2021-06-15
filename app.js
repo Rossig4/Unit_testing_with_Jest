c30e4156314ad0d34bca41010eb8426d935bcac5
Rossig4 <r.silvera.ag@gmail.com>
2021-06-15 19:12:54 +0000

    ejercicio listo

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 console.log("Hello World")
-const sum = (a,b) => {
-    return a + b
+
+const fromEuroToDollar = function(valueInEuro){
+    let valueInDollar = valueInEuro * 1.2;
+    return valueInDollar;
 }
-console.log(sum(7,3))
 
-module.exports = { sum };
+console.log(fromEuroToDollar(100))
 
+const fromDollarToYen = function(valueInDollar){
+    let valueInYen = (valueInDollar / 1.2) * 127.9;
+    return valueInYen;
+}
 
+console.log(fromDollarToYen(100))
+
+const fromYenToPound = function(valueInYen){
+    let valueInPound = (valueInYen / 127.9) * 0.8;
+    return valueInPound;
+}
+console.log(fromYenToPoun(100))
+
+module.exports = { fromEuroToDollar ,fromDollarToYen ,fromYenToPound}
 
--- test.js
+++ test.js
@@ -1,18 +1,30 @@
-// importar la función sum del archivo app.js
 
-const { sum } = require('./app.js');
+test("One euro should be 1.206 dollars", function(){
+    const { fromEuroToDollar } = require("./app.js")
 
-// comienza tu primera prueba
-test('adds 14 + 9 to equal 23', () => {
-    //dentro de la prueba llamamos a nuestra función sum con 2 números
-    let total = sum(14, 9);
-    
-    return a + b
-    const sum = (a,b) => {
-}
-    console.log(sum(14,9))
-    
+    const dollars = fromEuroToDollar(3.5)
 
-    // esperamos que la suma de esos 2 números sea 23
-    expect(total).toBe(23);
-});
+    const expected = 3.5 * 1.2;
+
+    expected(exppected).toBe(dollars);
+})
+
+test("1 dolar a yenes", function(){
+    const { fromDollarToYen } = require("./app.js")
+
+    const Yen = fromDollarToYen(1)
+
+    const expected = (1/1.2) * 127.9;
+
+    expect(expected).toBe(Yen);
+})
+
+test("1 Yen a Pound", function(){
+    const { fromYenToPound } = require("app.js")
+
+    const Pound = fromYenToPound(1)
+
+    const expected = (1/127.9) * 0.8;
+
+    expect(expected).toBe(Pound);
+})
